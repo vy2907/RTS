930fca649d5d6da647358b80583826c85dce3f55
vy2907 <vynguyen29071997@gmail.com>
2021-08-17 20:51:06 +0700

    first commit

---
+++ rts/src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDom from "react-dom";
+
+const App = () => {
+    return (
+    <div>
+        <h1>HI</h1>
+    </div>
+    );
+};
+
+ReactDom.render( <App />,document.querySelector('#root'));
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom';
-
-const App = () => {
-    return <div>
-        <h1>Hi</h1>
-    </div>
-}
-
-ReactDOM.render(<App />, document.querySelector('#root'));
