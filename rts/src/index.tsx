9664ebcdbc5601c10a5d9205d44aebba0804d9fb
vy2907 <vynguyen29071997@gmail.com>
2021-08-17 22:51:37 +0700

    10

--- rts/src/index.tsx
+++ rts/src/index.tsx
@@ -1,9 +1,10 @@
 import ReactDom from "react-dom";
+import GuestList from "./state/GuestList";
 
 const App = () => {
     return (
     <div>
-        <h1>HI</h1>
+        <GuestList />
     </div>
     );
 };
---
+++ rts/src/props/Parent.tsx
@@ -0,0 +1,8 @@
+import { ChildAsFC } from './Child';
+
+const Parent = () => {
+    return <ChildAsFC color="red"  onClick={() => console.log('Click')}>
+    </ChildAsFC>;
+};
+
+export default Parent;
