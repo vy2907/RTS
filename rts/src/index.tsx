0b8f15a5af54e057635350cac2a562f2d18e0979
vy2907 <vynguyen29071997@gmail.com>
2021-08-18 23:15:27 +0700

    13

--- rts/src/index.tsx
+++ rts/src/index.tsx
@@ -1,10 +1,10 @@
 import ReactDom from "react-dom";
-import GuestList from "./state/GuestList";
+import UserSearch from "./state/UserSearch";
 
 const App = () => {
     return (
     <div>
-        <GuestList />
+        <UserSearch />
     </div>
     );
 };
